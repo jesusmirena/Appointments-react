9eeedff8ccc6d0cbf7b2ea9ad3a94074b16b0b64
jesusmirena <jesusamh16@gmail.com>
2022-02-08 00:29:03 -0500

    Showing a different header if no patients

--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -3,20 +3,31 @@ import Patient from "./Patient";
 const PatientList = ({ patients }) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      <h2 className="font-black text-3xl text-center">Patient List</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-        Manage your{" "}
-        <span className="text-indigo-600 font-bold">
-          patients and appointments
-        </span>
-      </p>
-
-      {patients ? (
-        patients.map((patient) => (
-          <Patient key={patient.id} patient={patient} />
-        ))
+      {patients && patients.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Patient List</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Manage your{" "}
+            <span className="text-indigo-600 font-bold">
+              patients and appointments
+            </span>
+          </p>
+          {patients.map((patient) => (
+            <Patient key={patient.id} patient={patient} />
+          ))}
+        </>
       ) : (
-        <h2>There are no patients</h2>
+        <>
+          <h2 className="font-black text-3xl text-center">
+            There are no patients
+          </h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            start by adding patients and{" "}
+            <span className="text-indigo-600 font-bold">
+              they will appear in this place
+            </span>
+          </p>
+        </>
       )}
     </div>
   );
